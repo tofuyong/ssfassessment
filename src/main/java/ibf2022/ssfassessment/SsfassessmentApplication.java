ba86b67f7868b83e1bdced743f356c931eedd3a6
your_name <andreayong@Andreas-MacBook-Air.local>
2023-03-03 09:25:11 +0800

    first commit

---
+++ src/main/java/ibf2022/ssfassessment/SsfassessmentApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.ssfassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfassessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibf2022/ssfassessment/SsfassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.ssfassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
